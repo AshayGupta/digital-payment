7d43ff2ab150a609abc8646c5c54e6e4e5aeb314
Ashay Gupta <ashaygupta10@gmail.com>
2023-02-10 19:18:53 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Digital Payment',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
